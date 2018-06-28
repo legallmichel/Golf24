a381e83340734e02362abc8e1d8602bf1ed1843d
legallmichel <40575536+legallmichel@users.noreply.github.com>
2018-06-28 15:34:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+[sourcecode language= »javascript »]
+function digitCheck(x,y) {
+var digitsOld = splitToArray(x),
+digitsNew = splitToArray(y);
+for (var i = 0, c = digitsNew.length; i < c; i++){
+if (digitsNew[i] != digitsOld[i]){
+animateDigit(i, digitsOld[i], digitsNew[i]);
+}
+}
+}
+[/sourcecode]
