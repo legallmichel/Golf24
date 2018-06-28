35cc9979d45d1b72408529e23908ccd2538e3c8f
legallmichel <40575536+legallmichel@users.noreply.github.com>
2018-06-28 15:55:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// On récupère l’élément ‘note’ dans lequel on va inscrire des informations
+var note = $(‘#note’);
+// Création de l’objet ‘date’ (année / mois / jour) ici initialisé au : 9 mars 2012
+// Attention les mois commencent à 0 !
+var ts = new Date(2012, 2, 9);
+
+$(‘#countdown’).countdown({
+timestamp	: ts,
+callback	: function(days, hours, minutes, seconds){
+…
+}
+});
+
 function digitCheck(x,y) {
 var digitsOld = splitToArray(x),
 digitsNew = splitToArray(y);
@@ -7,3 +20,17 @@ animateDigit(i, digitsOld[i], digitsNew[i]);
 }
 }
 }
+var message = "";
+message += days + " jour" + ( days==1 ?  »:’s’ ) + ", ";
+message += hours + " heur" + ( hours==1 ?  »:’s’ ) + ", ";
+message += minutes + " minute" + ( minutes==1 ?  »:’s’ ) + " and ";
+message += seconds + " seconde" + ( seconds==1 ?  »:’s’ ) + " <br />";
+
+if (newYear) {
+message += "avant l’événement !";
+}
+else {
+message += "Il ne reste plus 10 jours à partir de maintenant !";
+}
+
+note.html(message);
