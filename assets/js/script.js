08b6f1c44eb5bf907017e7abcdf70ed707356b6d
legallmichel <40575536+legallmichel@users.noreply.github.com>
2018-06-29 10:01:00 +0200

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,36 @@
-// On récupère l’élément ‘note’ dans lequel on va inscrire des informations
-var note = $(‘#note’);
-// Création de l’objet ‘date’ (année / mois / jour) ici initialisé au : 9 mars 2012
-// Attention les mois commencent à 0 !
-
-var ts = new Date(2012, 2, 9);
-$(‘#countdown’).countdown({
-timestamp       : ts,
-callback        : function(days, hours, minutes, seconds){
-…
-}
+$(function(){
+	
+	var note = $('#note'),
+		ts = new Date(2012, 0, 1),
+		newYear = true;
+	
+	if((new Date()) > ts){
+		// The new year is here! Count towards something else.
+		// Notice the *1000 at the end - time must be in milliseconds
+		ts = (new Date()).getTime() + 10*24*60*60*1000;
+		newYear = false;
+	}
+		
+	$('#countdown').countdown({
+		timestamp	: ts,
+		callback	: function(days, hours, minutes, seconds){
+			
+			var message = "";
+			
+			message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+			message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+			message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+			message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+			
+			if(newYear){
+				message += "left until the new year!";
+			}
+			else {
+				message += "left to 10 days from now!";
+			}
+			
+			note.html(message);
+		}
+	});
+	
 });
-
-var message = "";
-message += days + " jour" + ( days==1 ?  »:’s’ ) + ", ";
-message += hours + " heur" + ( hours==1 ?  »:’s’ ) + ", ";
-message += minutes + " minute" + ( minutes==1 ?  »:’s’ ) + " and ";
-message += seconds + " seconde" + ( seconds==1 ?  »:’s’ ) + " <br />";
-
-if (newYear) {
-message += "avant l’événement !";
-}
-else {
-message += "Il ne reste plus 10 jours à partir de maintenant !";
-}
-
-note.html(message);
